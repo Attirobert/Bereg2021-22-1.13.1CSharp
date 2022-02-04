2c00222c6703c2f5e8857087d969b85bf23c102b
Attirobert <karobatti@gmail.com>
2022-02-04 11:40:34 +0100

    Órai

---
+++ Mtable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+
+            // Ha kapok számot a parancssorból
+            if (args.Length > 0)
+            {
+                // akkor átalakítom
+                szam = int.Parse(args[0]);
+            }
+            else
+            {
+                // ha nem, akkor generálok
+                Random rnd = new Random();
+                szam = rnd.Next(1, 50);
+            }
+
+            // Kiíratom az eredményt
+            Console.WriteLine(szam * 2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Mtable2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Parancssori paraméterek száma lehet: 0, 1, 2 */
+            int param = args.Length;  // Paraméterek száma
+            int szam1 = 0;  // 1. paraméter értéke
+            int szam2 = 0;  // 2. paraméter értéke
+
+            Random rnd = new Random();  // Véletlenszám generáló objektum példányosítása
+
+            // A számolás tagjainak beállítása
+            switch (param)
+            {
+                case 0: // Paraméterek száma: 0
+                    szam1 = rnd.Next(1, 100);   // Generáljuk az első paramétert
+                    szam2 = rnd.Next(1, 100);   // Generáljuk a második paramétert
+                    break;
+
+                case 1: // Paraméterek száma: 1
+                    szam1 = int.Parse(args[0]); // A stringként kapott paramétert átalakítjuk egésszé
+                    szam2 = rnd.Next(1, 100);
+                    break;
+
+                case 2: // Paraméterek száma: 2
+                    szam1 = int.Parse(args[0]);
+                    szam2 = int.Parse(args[1]);
+                    break;
+            }
+
+            // Kiíratom az eredményt
+            Console.WriteLine(szam1 * szam2);
+            Console.ReadKey();
+        }
+    }
+}
