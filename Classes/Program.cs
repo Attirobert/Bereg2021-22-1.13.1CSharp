4cd93cf5611add2585074692d16b1fdd07eb9cc5
Attirobert <karobatti@gmail.com>
2022-02-28 10:50:49 +0100

    Classes nincs befejezve

---
+++ Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Allat
+    {
+        // Osztályváltozók (properties)
+        private string nev;
+        private string fajta;
+        private string szin;
+        private int kor;
+
+        public Allat(string nev, string fajta, string szin)
+        {
+            this.nev = nev;
+            this.fajta = fajta;
+            this.szin = szin;
+        }
+
+        public string getNev()
+        {
+            return this.nev;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Példányosítunk objektumokat
+            Allat allat1 = new Allat("Bodri", "Puli", "fekete");
+            Console.WriteLine("Az állat1 neve: {0}", allat1.getNev()); 
+            
+            Allat allat2 = new Allat("Bolhás", "Komondor", "fehér");
+            Console.WriteLine("Az állat2 neve: {0}", allat2.getNev());
+            Console.ReadKey();
+        }
+    }
+}
