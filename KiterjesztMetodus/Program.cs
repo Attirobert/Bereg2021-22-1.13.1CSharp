868a1f40aee3ed0cd63131fb68e7838b1a2dacb5
Attirobert <karobatti@gmail.com>
2022-03-23 09:30:42 +0100

    Kiterjesztett metódus

---
+++ KiterjesztMetodus/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiterjesztMetodus
+{
+    /* A StringHelper egy burkoló osztály a string kezeléshez.
+     * Ezt bővítjük a Print metódussal
+     * Mind a befogadó osztálynak, mind a kiterjesztett metódusnak "static public" jelölőkkel kell rendelkeznie.
+     * Fontos még a metódus paraméterénél a "this" használata
+     */
+    static public class StringHelper
+    {
+        static public void Print(this string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "ezegystring";
+            Console.WriteLine("s.Print()");
+            s.Print();              // így is használhatjuk
+            Console.WriteLine("\nStringHelper.Print(s)");
+            StringHelper.Print(s);  // így is használhatjuk
+            Console.ReadKey();
+        }
+    }
+}
