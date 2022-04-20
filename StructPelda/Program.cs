27b58e135ad4082ddf95abf74927a26a39664e60
Attirobert <karobatti@gmail.com>
2022-04-20 10:49:23 +0200

    Struct befejezés

---
+++ StructPelda/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPelda
+{
+    class Program
+    {
+        struct Point
+        {
+            int x;
+            public int X
+            {
+                get { return x; }
+                set { x = value; }
+            }
+            int y;
+            public int Y
+            {
+                get { return y; }
+                set { y = value; }
+            }
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        struct Line
+        {
+            Point a;
+            public Point A
+            {
+                get { return a; }
+                set { a = value; }
+            }
+            Point b;
+            public Point B
+            {
+                get { return b; }
+                set { b = value; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Line l = new Line();
+            l.A = new Point(10, 10);
+            l.B = new Point(20, 20);
+            //l.A.X = 5;
+            Console.WriteLine("Pont koordináták A: {0},{1}; B: {2},{3}", l.A.X, l.A.Y, l.B.X, l.B.Y);
+            Console.ReadKey();
+        }
+    }
+}
--- Structura/Program.cs
+++ Structura/Program.cs
@@ -11,39 +11,54 @@ namespace Structura
         public int x;
     }    
     
-    struct Test2
+    struct Test1
     {
         public int x;
     }
 
-    struct Test3
+    
+    // Konstruktor
+    //    - 
+    struct Test2
     {
         public int x = 10; // nem kaphat kezdeti értéket
         public int y;
 
-        public Test3() { }  // Nem jó, kötelező a paraméter, elhagyjuk, de meghívható
-        public Test3(int x, int y) // ez sem jó, x nem kap értéket
+        public Test2() { }  // Nem jó, kötelező a paraméter; elhagyjuk, de meghívható
+        public Test2(int x, int y) // ez sem jó, x nem kap értéket
         {
-            this.y = y; 
+            this.y = y;
         }
     }
+
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            // new nélkül
-            Test s;
-            s.x = 3;
-            Console.WriteLine(s.x); // x == 0
+            // Létrehozás new nélkül
+            Test t0;
+            t0.x = 3;
+            Console.WriteLine("t0.x = {0}", t0.x); // x == 0
             Console.ReadKey();
 
-            Test t = new Test();
-            Console.WriteLine(t.x); // x == 0
+            // Létrehozás new-val
+            Test1 t1 = new Test1();
+            Console.WriteLine("t1.x = {0}", t1.x); // x == 0
             Console.ReadKey();
 
-            Test t2;
-            Console.WriteLine(t2.x); // x nem kapott értéket
+            // x nem kapott értéket
+            Test2 t2;
+            Console.WriteLine("t2.x = {0}", t2.x); 
+            Console.ReadKey();
+
+            // Érték típusú 
+            Test t3, t4;
+            t3.x = 10;
+            t4 = t3;
+            t3.x = 221;
+            Console.WriteLine("t3.x = {0}; t4.x = {1}", t3.x, t4.x);
             Console.ReadKey();
         }
     }
