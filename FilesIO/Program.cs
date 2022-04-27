55ee5555de1dde1928b4d66f7e24e5ba75a5cbab
Attila Róbert Kovács <kovacs.attila.robert@dszcberegszaszi.hu>
2022-04-27 20:52:05 +0200

    File kezelés

---
+++ FilesIO/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesIO
+{
+    class FilesIO
+    {
+        // Osztályváltozók
+        private string fileName;
+        private FileMode fileMode;
+        private FileAccess fileAccess;
+        private FileStream fs;
+        private StreamReader sr;
+
+
+        public FilesIO() {
+            setFileName();
+            setFileMode(); 
+            setFileAccess();
+        }
+
+        private void setFileName()
+        {
+            Console.WriteLine("Adja meg a fájl nevét kiterjesztés nélkül:");
+            this.fileName = Console.ReadLine() + ".txt";
+        }
+
+        private void setFileMode()
+        {
+            bool ismet;
+            string s;
+
+            Console.WriteLine("Adja meg a műveletet:");
+            Console.WriteLine("     cr:  Create");
+            Console.WriteLine("     crn: CreateNew");
+            Console.WriteLine("     o:   Open");
+            Console.WriteLine("     oc:  OpenOrCreate");
+            Console.WriteLine("     a:   Append");
+            Console.WriteLine("     t:   Truncate");
+
+            do
+            {
+                ismet = false;
+                s = Console.ReadLine();
+                switch (s)
+                {
+                    case "cr": fileMode = FileMode.Create; break;       // Létrehoz egy új fájlt, ha már létezik, a tartalmát kitörli.
+                    case "crn": fileMode = FileMode.CreateNew; break;   // Mint előbb, de ha már létezik a fájl, akkor kivételt dob.
+                    case "o": fileMode = FileMode.Open; break;          // Megnyit egy fájlt, ha nem létezik, kivételt dob.
+                    case "oc": fileMode = FileMode.OpenOrCreate; break; // Mintelőbb, de ha nem létezik, akkor létrehozza a fájlt.
+                    case "a": fileMode = FileMode.Append; break;        // Megnyit egy fájlt és automatikusan a végére áll. Ha nem létezik, létrehozza.
+                    case "t": fileMode = FileMode.Truncate; break;      // Megnyit egy létező fájlt és törli a tartalmát.  Ebben a módban a fájl  tartalmát nem lehet olvasni (egyébként kivételt dob).
+                    default:
+                        Console.WriteLine("Hibás adat! Ismételje meg!");
+                        ismet = true;
+                        break;
+                }
+            } while (ismet);
+        }
+
+        private void setFileAccess()
+        {
+            bool ismet;
+            string s;
+
+            Console.WriteLine("Adja meg a fájl hozzáférést:");
+            Console.WriteLine("     r:  Olvasás");
+            Console.WriteLine("     w:  Írás");
+            Console.WriteLine("     rw: Olvasás és írás");
+
+            do
+            {
+                ismet = false;
+                s = Console.ReadLine();
+                switch (s)
+                {
+                    case "r": fileAccess = FileAccess.Read; break;
+                    case "w": fileAccess = FileAccess.Write; break;
+                    case "rw": fileAccess = FileAccess.ReadWrite; break;
+                    default:
+                        Console.WriteLine("Hibás adat! Ismételje meg!");
+                        ismet = true;
+                        break;
+                }
+            } while (ismet);
+        }
+
+        public StreamReader getFile()
+        {
+            this.fs = new FileStream(this.fileName, this.fileMode, this.fileAccess);
+            this.sr = new StreamReader(fs);
+            return sr;
+        }
+
+        public void closeFile()
+        {
+            if (this.sr != null) this.sr.Close();
+            if (this.fs != null) this.fs.Close();
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FilesIO f = new FilesIO();
+            StreamReader sr = f.getFile();
+
+            // Dolgozunk a fájllal
+            string s = sr.ReadLine(); 
+            while (s != null) 
+            { 
+                Console.WriteLine(s); 
+                s = sr.ReadLine(); 
+            }
+
+            Console.ReadKey();
+            f.closeFile();
+        }
+    }
+}
