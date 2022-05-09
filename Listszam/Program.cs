047201dd8854155e3721658a24091263849b4482
Attila Róbert Kovács <kovacs.attila.robert@dszcberegszaszi.hu>
2022-05-09 10:54:17 +0200

    Lista

---
+++ Listszam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ metódusok:
+-setHatar:Consolrol egy természetes számként beolvasom az elemekszámát, le kell ellenőrizni, hogy tényleg szám-e.
+-setTort: Ennek az a feladata, hogy beolvas egy törtszámot.(tizedespont, perjel, tizedesvessző). Ha "." --> alakítsuk vesszővé; ha perjel, akkor tizedesszámmá és adja hozzá a listához. Írja ki mindig a beolvasott elemek számát és mennyit lehet még beolvasni. Hogyha az olvasás elérte a max mennyiséget, jelezze ki a felhasználónak és ha többet akar beadni, akkor ne fogadja el. 
+-getLista: kiírja a consolra a számokat vesszővel elválasztva.
+-getAtlag: A beolvasott számok átlagát kiírja a consolra.
+-getOsszeg: Beolvasott számok összegét kiírja a consolra.
+ 
+ */
+
+namespace Listszam
+{
+    class ListSzam
+    {
+        private List<double> lista;
+        private int listhossz;
+        public ListSzam()
+        {
+            lista = new List<double>();
+        }
+        public void setHatar()
+        {
+            string 
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
