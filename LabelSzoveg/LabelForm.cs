913da10602f6f180ba80a00c083d32fa4484f7b2
Attirobert <karobatti@gmail.com>
2022-05-26 10:53:45 +0200

    Label Igazítás

---
+++ LabelSzoveg/LabelForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelSzoveg
+{
+    public partial class LabelForm : Form
+    {
+        public LabelForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LabelForm_Load(object sender, EventArgs e)
+        {
+            lblFull.Text = "Kattints a formon valahova!";
+        }
+
+        private void lblFull_MouseClick(object sender, MouseEventArgs e)
+        {
+            int o = (int)e.X / (lblFull.Width / 3);     // Hanyadik oszlopba kattintottunk
+            int s = (int)e.Y / (lblFull.Height / 3);    // Hanyadik sorba kattintottunk
+        }
+    }
+}
