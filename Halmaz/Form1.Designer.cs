38fa131a444a797be35339ba151bca05539ac099
Attirobert <karobatti@gmail.com>
2022-06-01 11:40:51 +0200

    Halmaz

---
+++ Halmaz/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace Halmaz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxA = new System.Windows.Forms.ListBox();
+            this.lbxB = new System.Windows.Forms.ListBox();
+            this.lbxAuB = new System.Windows.Forms.ListBox();
+            this.lbxAmB = new System.Windows.Forms.ListBox();
+            this.lbxAbolB = new System.Windows.Forms.ListBox();
+            this.lbxBbolA = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nudA = new System.Windows.Forms.NumericUpDown();
+            this.nudB = new System.Windows.Forms.NumericUpDown();
+            this.btnOk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbxA
+            // 
+            this.lbxA.FormattingEnabled = true;
+            this.lbxA.Location = new System.Drawing.Point(46, 50);
+            this.lbxA.Name = "lbxA";
+            this.lbxA.Size = new System.Drawing.Size(108, 303);
+            this.lbxA.TabIndex = 0;
+            // 
+            // lbxB
+            // 
+            this.lbxB.FormattingEnabled = true;
+            this.lbxB.Location = new System.Drawing.Point(168, 50);
+            this.lbxB.Name = "lbxB";
+            this.lbxB.Size = new System.Drawing.Size(108, 303);
+            this.lbxB.TabIndex = 0;
+            // 
+            // lbxAuB
+            // 
+            this.lbxAuB.FormattingEnabled = true;
+            this.lbxAuB.Location = new System.Drawing.Point(290, 50);
+            this.lbxAuB.Name = "lbxAuB";
+            this.lbxAuB.Size = new System.Drawing.Size(108, 303);
+            this.lbxAuB.TabIndex = 0;
+            // 
+            // lbxAmB
+            // 
+            this.lbxAmB.FormattingEnabled = true;
+            this.lbxAmB.Location = new System.Drawing.Point(412, 50);
+            this.lbxAmB.Name = "lbxAmB";
+            this.lbxAmB.Size = new System.Drawing.Size(108, 303);
+            this.lbxAmB.TabIndex = 0;
+            // 
+            // lbxAbolB
+            // 
+            this.lbxAbolB.FormattingEnabled = true;
+            this.lbxAbolB.Location = new System.Drawing.Point(534, 50);
+            this.lbxAbolB.Name = "lbxAbolB";
+            this.lbxAbolB.Size = new System.Drawing.Size(108, 303);
+            this.lbxAbolB.TabIndex = 0;
+            // 
+            // lbxBbolA
+            // 
+            this.lbxBbolA.FormattingEnabled = true;
+            this.lbxBbolA.Location = new System.Drawing.Point(656, 50);
+            this.lbxBbolA.Name = "lbxBbolA";
+            this.lbxBbolA.Size = new System.Drawing.Size(108, 303);
+            this.lbxBbolA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(287, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "A unio B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(409, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "A metszet B";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(531, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "A-B";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(653, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "B-A";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(43, 383);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "A halmazok elemszáma";
+            // 
+            // nudA
+            // 
+            this.nudA.Location = new System.Drawing.Point(46, 412);
+            this.nudA.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudA.Name = "nudA";
+            this.nudA.Size = new System.Drawing.Size(120, 20);
+            this.nudA.TabIndex = 3;
+            this.nudA.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // nudB
+            // 
+            this.nudB.Location = new System.Drawing.Point(186, 412);
+            this.nudB.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudB.Name = "nudB";
+            this.nudB.Size = new System.Drawing.Size(120, 20);
+            this.nudB.TabIndex = 3;
+            this.nudB.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(376, 409);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "Előállít";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.nudB);
+            this.Controls.Add(this.nudA);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxBbolA);
+            this.Controls.Add(this.lbxAbolB);
+            this.Controls.Add(this.lbxAmB);
+            this.Controls.Add(this.lbxAuB);
+            this.Controls.Add(this.lbxB);
+            this.Controls.Add(this.lbxA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxA;
+        private System.Windows.Forms.ListBox lbxB;
+        private System.Windows.Forms.ListBox lbxAuB;
+        private System.Windows.Forms.ListBox lbxAmB;
+        private System.Windows.Forms.ListBox lbxAbolB;
+        private System.Windows.Forms.ListBox lbxBbolA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nudA;
+        private System.Windows.Forms.NumericUpDown nudB;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ Halmaz/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Halmaz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+           
+            //A halmaz generálása
+            Halmazgen(lbxA, nudA);
+            //B halmaz generálása
+            Halmazgen(lbxB, nudB);
+            //Metszet
+            Metszet(lbxAmB, lbxA, lbxB);
+            //Unió
+            Unio(lbxAuB, lbxA, lbxB);
+            //A-B
+            AbolB(lbxAbolB, lbxA, lbxB);
+            //B-A
+            BbolA(lbxBbolA, lbxA, lbxB);
+        }
+
+        private void BbolA(ListBox lbxBbolA, ListBox lbxA, ListBox lbxB)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AbolB(ListBox lbxAbolB, ListBox lbxA, ListBox lbxB)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Unio(ListBox lbxAuB, ListBox lbxA, ListBox lbxB)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Metszet(ListBox lbxAmB, ListBox lbxA, ListBox lbxB)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Halmazgen(ListBox lbxB, NumericUpDown nudB)
+        {
+            Random rnd = new Random();
+            
+           
+        }
+    }
+}
